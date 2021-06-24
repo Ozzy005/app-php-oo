d7eef155ebb940595188e6821371373060dc0552
Rafael Arend <rafinhaarend123@hotmail.com>
2021-06-24 11:30:27 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,62 @@
+<?php
+date_default_timezone_set('America/Sao_Paulo');
+
+if (version_compare(PHP_VERSION, '7.0.0') == -1)
+{
+    die('A versão mínima do PHP para rodar esta aplicação é: 7.0.0');
+}
+
+// Lib loader
+require_once 'Lib/Livro/Core/ClassLoader.php';
+$al= new Livro\Core\ClassLoader;
+$al->addNamespace('Livro', 'Lib/Livro');
+$al->register();
+
+// App loader
+require_once 'Lib/Livro/Core/AppLoader.php';
+$al= new Livro\Core\AppLoader;
+$al->addDirectory('App/Control');
+$al->addDirectory('App/Model');
+$al->register();
+
+// Vendor
+$loader = require 'vendor/autoload.php';
+$loader->register();
+
+use Livro\Session\Session;
+
+$content = '';
+
+new Session;
+if (Session::getValue('logged')) {
+    $template = file_get_contents('App/Templates/template.html');
+    $class = '';
+}
+else {
+    $template = file_get_contents('App/Templates/login.html');
+    $class = 'LoginForm';
+}
+
+if (isset($_GET['class']) AND Session::getValue('logged'))
+{
+    $class = $_GET['class'];
+}
+
+if (class_exists($class))
+{
+    try
+    {
+        $pagina = new $class;
+        ob_start();
+        $pagina->show();
+        $content = ob_get_contents();
+        ob_end_clean();
+    }
+    catch (Exception $e)
+    {
+        $content = $e->getMessage() . '<br>' .$e->getTraceAsString();
+    }
+}
+$output = str_replace('{content}', $content, $template);
+$output = str_replace('{class}',   $class, $output);
+echo $output;
